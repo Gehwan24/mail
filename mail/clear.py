a6aec88b83c6756c12f6764fe11093d0ecb71405
Gehwan24 <53293830+Gehwan24@users.noreply.github.com>
2019-08-01 17:03:58 +0900

    Update clear.py

--- mail/clear.py
+++ mail/clear.py
@@ -1,9 +1,13 @@
 if __name__  == "clear":
     with open('mw24_title.txt', mode='w') as title_file:
         pass
-    with open('mw24_pop_title.txt', mode='w') as title_file_temp:
+    with open('gov24_title.txt', mode='w') as title_file_tem:
+        pass
+    with open('iros_title.txt', mode='w') as title_file_tem:
         pass
     with open('gov24_title.txt', mode='w') as title_file_tem:
         pass
-        
+    with open('hikorea_title.txt', mode='w') as title_file_tem:
+        pass
+
 
