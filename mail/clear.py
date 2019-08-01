16fb2336d0ac3ac77666e647a3663eb84ef59446
Gehwan24 <53293830+Gehwan24@users.noreply.github.com>
2019-08-01 16:58:18 +0900

    Add files via upload

---
+++ mail/clear.py
@@ -0,0 +1,9 @@
+if __name__  == "clear":
+    with open('mw24_title.txt', mode='w') as title_file:
+        pass
+    with open('mw24_pop_title.txt', mode='w') as title_file_temp:
+        pass
+    with open('gov24_title.txt', mode='w') as title_file_tem:
+        pass
+        
+
