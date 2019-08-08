8526fdb0ad2a6b737cc3677ea77479614be06e3a
Gehwan24 <53293830+Gehwan24@users.noreply.github.com>
2019-08-08 15:30:46 +0900

    Update run.py

--- mail/run.py
+++ mail/run.py
@@ -6,22 +6,23 @@ import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 def clear():
-   command = "python clear.py"
+   command = "python -m clear.py"
    subprocess.call(command.split())
 
 def once():
-   command = "python once.py"
+   command = "python -m once.py"
    subprocess.call(command.split())
 
 def main():
-   command = "python main.py"
+   command = "python -m main.py"
    subprocess.call(command.split())
 
 if __name__ == "__main__":
    logging.basicConfig(level=logging.INFO)
    sched = BlockingScheduler()
    sched.add_job(clear, 'cron', hour="0", minute="30")
-   sched.add_job(once, 'cron', hour="5", minute="20")
+   sched.add_job(once, 'cron', hour="17", minute="20")
    sched.add_job(main, 'cron', hour="8-23", minute="59")
    sched.start()
    
+  
